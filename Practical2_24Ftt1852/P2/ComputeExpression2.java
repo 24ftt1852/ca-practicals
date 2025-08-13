954a23f58989bde6ff0d4ca5a409d2770ded02af
24ftt1852 <24FTT1852@student.pb.edu.bn>
2025-08-13 22:36:36 +0800

    Add files via upload

---
+++ Practical2_24Ftt1852/P2/ComputeExpression2.java
@@ -0,0 +1,16 @@
+package P2;
+
+public class ComputeExpression2 {
+    
+    public static void main(String[] args) {
+        
+        double result1 = 7.5 * 2.5 - 1.5 * 3;
+        double result2 = result1 / (4 - 2.5);
+
+        System.out.println("The result of the" );
+        System.out.println("         7.5 x 2.5 - 1.5 x 3");
+        System.out.println("----------------------------------- is "+ result2);
+        System.out.println("               4 - 2.5");
+
+    }
+}
---
+++ Practical2_24Ftt1852/P2/DisplayTwoMessages.java
@@ -0,0 +1,9 @@
+package P2;
+
+public class DisplayTwoMessages {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to AD2204 Introduction to Programming");
+        System.out.println("This is Practical 2");
+    }
+}
